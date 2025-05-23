5f3aaecc2ec0b4f833a0d68733ab2274e22fa704
svc-accounting <svc-accountingsvcs@UTA.edu>
2025-05-23 14:20:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def generate_hub():
     return jsonify({'message': 'HUB report generated', 'file': output_file}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
+
